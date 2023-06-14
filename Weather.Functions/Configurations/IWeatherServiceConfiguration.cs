de71ad7cff50f61449a96a928d00fdfbfdba683f
Navideh Saghiri <extnasag@pandora.net>
2023-06-14 21:18:15 +0200

    Add configurations for open weather API

---
+++ Weather.Functions/Configurations/IWeatherServiceConfiguration.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Weather.Functions.Configurations
+{
+    /// <summary>
+    /// Open Weather API Configuration 
+    /// </summary>
+    public interface IWeatherServiceConfiguration
+    {
+        /// <summary>
+        /// The base url to connect to openWeatherAPI
+        /// </summary>
+        string ServiceBaseUrl { get; }
+
+        /// <summary>
+        /// API key to connect to openWeatherAPI
+        /// </summary>
+        string ApiKey { get; }
+    }
+}
---
+++ Weather.Functions/Configurations/WeatherServiceConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Weather.Functions.Configurations
+{
+    public class WeatherServiceConfiguration : IWeatherServiceConfiguration
+    {
+        private readonly IConfiguration _configuration;
+
+        public WeatherServiceConfiguration(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string ServiceBaseUrl => _configuration["OPEN_WEATHER_BASEURL"] ?? "https://api.openweathermap.org/data/2.5/";
+
+
+        public string ApiKey => _configuration["OPEN_WEATHER_APIKEY"] ?? string.Empty;
+    }
+}
