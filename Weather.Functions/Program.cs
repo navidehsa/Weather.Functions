c6f8dba1b6d92337482b0d110f25ec496b6fe401
Navideh Saghiri <extnasag@pandora.net>
2023-06-18 22:44:00 +0200

    Register services

--- Weather.Functions/Program.cs
+++ Weather.Functions/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Weather.Functions.Configurations;
+using Weather.Functions.Services;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureOpenApi()
+    .ConfigureServices(_ =>
+       {
+           _.AddHttpClient();
+           _.AddSingleton<IWeatherService, WeatherService>();
+           _.AddSingleton<IWeatherServiceConfiguration, WeatherServiceConfiguration>();
+           _.AddLogging();
+       })
     .Build();
 
 host.Run();
